5903521ab52aed7f1ebc85416f01a1f3b0420736
Levy4198 <58131144+Levy4198@users.noreply.github.com>
2022-02-06 17:14:34 +0300

    shifting database

--- initialize.php
+++ initialize.php
@@ -3,8 +3,10 @@ $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=
 if(!defined('base_url')) define('base_url','http://localhost/freelance/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
-if(!defined('DB_NAME')) define('DB_NAME',"db_freelance");
+if(!defined('DB_SERVER')) define('DB_SERVER',"us-cdbr-east-05.cleardb.net");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"b9490a9b679bdc");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"533af754");
+if(!defined('DB_NAME')) define('DB_NAME',"heroku_a7d81cb1e20e18a");
 ?>
+
+<!-- mysql://b9490a9b679bdc:533af754@us-cdbr-east-05.cleardb.net/heroku_a7d81cb1e20e18a?reconnect=true -->
